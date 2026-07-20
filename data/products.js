c3814db7a41e086b6652f8ab90064f6ade5fa066
Meghana <pareddysruthi46@gmail.com>
2026-07-20 14:31:23 +0530

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,1 +1,35 @@
+module.exports = [
 
+{
+id:1,
+name:"Laptop",
+price:55000,
+image:"laptop.png",
+description:"Powerful Laptop"
+},
+
+{
+id:2,
+name:"Smart Phone",
+price:25000,
+image:"mobile.png",
+description:"Android Phone"
+},
+
+{
+id:3,
+name:"Smart Watch",
+price:4500,
+image:"watch.png",
+description:"Fitness Watch"
+},
+
+{
+id:4,
+name:"Headphones",
+price:3000,
+image:"headphones.png",
+description:"Wireless Headphones"
+}
+
+];
